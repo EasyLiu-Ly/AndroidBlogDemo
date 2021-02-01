9180eab0800e2034b8b6699faf84b89557b8caee
easyliu <easyliu@tencent.com>
2021-02-01 12:07:10 +0800

    feat:add glide demo

--- glide_demo/src/main/java/com/easyliu/demo/glide_demo/MainActivity.kt
+++ glide_demo/src/main/java/com/easyliu/demo/glide_demo/MainActivity.kt
@@ -1,11 +1,25 @@
 package com.easyliu.demo.glide_demo
 
-import android.support.v7.app.AppCompatActivity
+import android.graphics.Color
+import android.graphics.drawable.ColorDrawable
 import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+import android.widget.ImageView
+import com.bumptech.glide.Glide
 
 class MainActivity : AppCompatActivity() {
+    private val img by lazy {
+        findViewById<ImageView>(R.id.iv_img)
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        Glide.with(this)
+                .load("http://i.gtimg.cn/qqlive/images/20191209/i1575881814_1.jpg")
+                .centerCrop()
+                .circleCrop()
+                .placeholder(ColorDrawable(Color.RED))
+                .into(img);
     }
 }
---
+++ glide_demo/src/main/java/com/easyliu/demo/glide_demo/MyGlideModule.java
@@ -0,0 +1,27 @@
+package com.easyliu.demo.glide_demo;
+
+import android.content.Context;
+import android.support.annotation.NonNull;
+
+import com.bumptech.glide.GlideBuilder;
+import com.bumptech.glide.annotation.GlideModule;
+import com.bumptech.glide.load.engine.cache.LruResourceCache;
+import com.bumptech.glide.module.AppGlideModule;
+
+@GlideModule
+public class MyGlideModule extends AppGlideModule {
+
+    @Override
+    public void applyOptions(@NonNull Context context,
+                             @NonNull GlideBuilder builder) {
+        super.applyOptions(context, builder);
+        //内存缓存相关,默认是24m
+        int memoryCacheSizeBytes = 1024 * 1024 * 20; // 20mb
+        builder.setMemoryCache(new LruResourceCache(memoryCacheSizeBytes));
+    }
+
+    @Override
+    public boolean isManifestParsingEnabled() {
+        return false;
+    }
+}
