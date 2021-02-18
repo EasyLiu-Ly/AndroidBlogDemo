65b7bddd0e6c517bacafca51cfa3be2495852b82
easyliu <easyliu@tencent.com>
2021-02-18 16:21:57 +0800

    feat:glide demo add extension

---
+++ glide_demo/src/main/java/com/easyliu/demo/glide_demo/MyAppGlideExtension.java
@@ -0,0 +1,24 @@
+package com.easyliu.demo.glide_demo;
+
+import androidx.annotation.NonNull;
+
+import com.bumptech.glide.annotation.GlideExtension;
+import com.bumptech.glide.annotation.GlideOption;
+import com.bumptech.glide.request.BaseRequestOptions;
+
+@GlideExtension
+public class MyAppGlideExtension {
+    // Size of mini thumb in pixels.
+    private static final int MINI_THUMB_SIZE = 100;
+
+    private MyAppGlideExtension() {
+    }
+
+    @NonNull
+    @GlideOption
+    public static BaseRequestOptions<?> miniThumb(BaseRequestOptions<?> options) {
+        return options
+                .fitCenter()
+                .override(MINI_THUMB_SIZE);
+    }
+}
--- glide_demo/src/main/java/com/easyliu/demo/glide_demo/MyGlideModule.java
+++ glide_demo/src/main/java/com/easyliu/demo/glide_demo/MyGlideModule.java
@@ -4,10 +4,14 @@ import android.content.Context;
 
 import androidx.annotation.NonNull;
 
+import com.bumptech.glide.Glide;
 import com.bumptech.glide.GlideBuilder;
+import com.bumptech.glide.Registry;
 import com.bumptech.glide.annotation.GlideModule;
 import com.bumptech.glide.load.engine.cache.LruResourceCache;
 import com.bumptech.glide.module.AppGlideModule;
+
+import org.jetbrains.annotations.NotNull;
 
 @GlideModule
 public class MyGlideModule extends AppGlideModule {
@@ -24,5 +28,12 @@ public class MyGlideModule extends AppGlideModule {
     @Override
     public boolean isManifestParsingEnabled() {
         return false;
+    }
+
+    @Override
+    public void registerComponents(@NonNull @NotNull Context context,
+                                   @NonNull @NotNull Glide glide,
+                                   @NonNull @NotNull Registry registry) {
+        super.registerComponents(context, glide, registry);
     }
 }
